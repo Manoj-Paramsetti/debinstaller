3af25f92a18390625e62ab33077845e7708ec81a
Manoj-Paramsetti <paramsetti.manoj@gmail.com>
2021-08-22 09:37:24 +0530

    black: code style formatted

--- setup.py
+++ setup.py
@@ -2,16 +2,19 @@ import os
 
 # Color
 def _success(text):
-    print('\033[1;35m'+text+'\033[0m')
+    print("\033[1;35m" + text + "\033[0m")
 
-if __name__ == '__main__':
 
-	if (os.path.isfile('bin/debinstalle')): 
-		pass
-		os.system('cp -r bin/debinstaller /usr/bin/debinstaller')
-	else:
-		pass
-		os.system('wget https://raw.githubusercontent.com/Manoj-Paramsetti/debinstaller/main/bin/debinstaller -P /usr/bin/')
-	os.system('chmod +x /usr/bin/debinstaller')
+if __name__ == "__main__":
 
-	_success('debinstaller is installed')
+    if os.path.isfile("bin/debinstaller"):
+        pass
+        os.system("cp -r bin/debinstaller /usr/bin/debinstaller")
+    else:
+        pass
+        os.system(
+            "wget https://raw.githubusercontent.com/Manoj-Paramsetti/debinstaller/main/bin/debinstaller -P /usr/bin/"
+        )
+    os.system("chmod +x /usr/bin/debinstaller")
+
+    _success("debinstaller is installed")
