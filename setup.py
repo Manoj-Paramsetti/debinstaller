1716d17661be9439432511dce6ea9b5134e06eef
Manoj-Paramsetti <paramsetti.manoj@gmail.com>
2021-08-22 09:25:21 +0530

    Remove comments and fix condition

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
 
 	if (os.path.isfile('bin/debinstalle')): 
 		pass
-		#os.system('cp -r bin/debinstaller /usr/bin/debinstaller')
+		os.system('cp -r bin/debinstaller /usr/bin/debinstaller')
 	else:
 		pass
-	#	os.system('wget https://raw.githubusercontent.com/Manoj-Paramsetti/debinstaller/main/bin/debinstaller -P /usr/bin/')
-	#os.system('chmod +x /usr/bin/debinstaller')
+		os.system('wget https://raw.githubusercontent.com/Manoj-Paramsetti/debinstaller/main/bin/debinstaller -P /usr/bin/')
+	os.system('chmod +x /usr/bin/debinstaller')
 
 	_success('debinstaller is installed')
